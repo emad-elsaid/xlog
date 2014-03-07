01b31fe1ca9a25affa3d2e2e7642c413530067d3
Emad Elsaid <blazeeboy@gmail.com>
2014-03-08 00:02:07 +0200

    add style and cancan ability
    margin before alert boxes
    rescue from unauthorized access exception
    cancan ability (allow everything to logged in user and read posts only
    for guests
    add alert box after notice box
    separated partial for flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /posts
   # GET /posts.json
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
+
+    unless user.nil?
+      can :manage, :all
+    else
+      can :read, Post
+    end
+    
     # The first argument to `can` is the action you are giving the user 
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
