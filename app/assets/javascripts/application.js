d29b765e63794407413f7886594e631982f3b3eb
Emad Elsaid <blazeeboy@gmail.com>
2014-03-08 22:53:04 +0200

    onpageupdate fire the autosize for text area make permalink sierct after the domain eg. domain.com/post-perma-link , this is perfect for SEO handle the tab key for text areas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,12 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ 
+var onPageUpdate = function(){
 	$(document).foundation(); 
 	$('textarea[data-expanding]').autosize();
-});
+}
+$(onPageUpdate);
+document.addEventListener("page:update", onPageUpdate);
+
+// cache pages 
+Turbolinks.enableTransitionCache();
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Xlog::Application.routes.draw do
   resources :posts
 
   devise_for :users
+  get ':id' => 'posts#show', as: 'post_link'
   root 'posts#index'
 end
