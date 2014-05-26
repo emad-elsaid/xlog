9471ca9897defc4eb27d06c2593b9eb000655b36
Emad Elsaid <blazeeboy@gmail.com>
2014-05-27 00:06:16 +0200

    application script fix and posts controller string separation

--- app/assets/javascripts/application.coffee
+++ app/assets/javascripts/application.coffee
@@ -4,11 +4,12 @@
 #= require turbolinks
 #= require_tree .
 
-$ ->
-  onPageUpdate = ->
-    $(document).foundation() 
-    $('textarea[data-expanding]').autosize()
+onPageUpdate = ->
+  $(document).foundation() 
+  $('textarea[data-expanding]').autosize()
 
+
+$(onPageUpdate)
 document.addEventListener 'page:update', onPageUpdate
 
 # cache pages 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, notice: t('post.add.success') }
         format.json { render action: 'show', status: :created, location: @post }
       else
         format.html { render action: 'new' }
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, notice: t('post.update.success') }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
