f7ef05ae99c4fab38edc90cd383a200f07a85ce5
Emad Elsaid <emad.abdelhay@tourlane.com>
2020-02-04 14:23:45 +0100

    remove custom css

--- cmd/xlog/templates.go
+++ cmd/xlog/templates.go
@@ -1,52 +1,12 @@
 package main
 
 const TEMPLATE = `
-{{define "style.css"}}
-html, body {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-}
-
-form.edit {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-}
-
-form.edit table {
-    width: 100%;
-    border-collapse: collapse;
-    border-spacing: 0;
-}
-
-form.edit table td {
-    padding: 0;
-}
-
-.collapse {
-    height: 0%;
-}
-
-.actions {
-    padding: 1em;
-    text-align: right;
-}
-
-.actions button {
-    font-size: 1.2em;
-    padding: 0.25em;
-}
-
-{{end}}
 {{define "view.html"}}
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.css">
-    <style>{{template "style.css"}}</style>
   </head>
   <body>
     <section class="section">
@@ -63,28 +23,29 @@ form.edit table td {
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-    <style>{{template "style.css"}}</style>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.css">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
 		<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
   </head>
 	<body>
-		<form method="POST" action="/{{ .action }}" class="edit">
-      <table>
-        <tr>
-          <td>
-			      <textarea name="content" autofocus>{{ .content }}</textarea>
-          </td>
-        </tr>
-        <tr class="collapse">
-          <td>
-            <div class="actions">
-              <button type="submit" >Save</button>
-            </div>
-          </td>
-        </tr>
-      </table>
-		</form>
-		<script> var simplemde = new SimpleMDE({autofocus: true, spellChecker: false}); </script>
+		<section class="section">
+				<form method="POST" action="/{{ .action }}" class="form">
+					<div class="field">
+						<div class="control has-text-right">
+							<button type="submit" class="button is-link">Save</button>
+						</div>
+					</div>
+					<div class="field">
+						<textarea name="content" autofocus>{{ .content }}</textarea>
+					</div>
+					<div class="field">
+						<div class="control has-text-right">
+							<button type="submit" class="button is-link">Save</button>
+						</div>
+					</div>
+				</form>
+		</section>
+		<script> var simplemde = new SimpleMDE({autofocus: true, spellChecker: false, toolbar: false}); </script>
 	</body>
 </html>
 {{end}}
