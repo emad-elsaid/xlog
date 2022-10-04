73e8a6e850f0672b89096bdfb8e0560385de1cfd
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2022-10-04 21:06:46 +0200

    Add Documentation for most types, vars, functions

--- main.go
+++ main.go
@@ -11,9 +11,14 @@ import (
 	"time"
 )
 
+// Template file name. its content is used as default value for the editor when
+// creating new pages.
 const TEMPLATE_NAME = "template"
 
 func main() {
+	// Uses current working directory as default value for source flag. If the
+	// source flag is set by user the program changes working directory to is
+	// and the rest of the program can use relative paths to access files
 	cwd, _ := os.Getwd()
 	source := flag.String("source", cwd, "Directory that will act as a storage")
 	flag.Parse()
@@ -27,18 +32,24 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Program Core routes. View, Edit routes and a route to write new content
+	// to the page. + handling root path which just show `index` page.
 	GET("/", RootHandler)
 	GET("/edit/{page:.*}", GetPageEditHandler)
 	GET("/{page:.*}", GetPageHandler)
 	POST("/{page:.*}", PostPageHandler)
 
+	// Start the server
 	START()
 }
 
+// Redirect to `/index` to render the index page.
 func RootHandler(w Response, r Request) Output {
 	return Redirect("/index")
 }
 
+// Shows a page. the page name is the path itself. if the page doesn't exist it
+// redirect to edit page otherwise will render it to HTML
 func GetPageHandler(w Response, r Request) Output {
 	vars := VARS(r)
 	page := NewPage(vars["page"])
@@ -52,13 +63,15 @@ func GetPageHandler(w Response, r Request) Output {
 		"title":     page.Emoji() + " " + page.Name,
 		"updated":   ago(time.Now().Sub(page.ModTime())),
 		"content":   template.HTML(page.Render()),
-		"tools":     renderWidget(TOOLS_WIDGET, &page, r),
-		"sidebar":   renderWidget(SIDEBAR_WIDGET, &page, r),
-		"meta":      renderWidget(META_WIDGET, &page, r),
-		"afterView": renderWidget(AFTER_VIEW_WIDGET, &page, r),
+		"tools":     renderWidget(TOOLS_WIDGET, &page, r),      // all tools registered widgets
+		"sidebar":   renderWidget(SIDEBAR_WIDGET, &page, r),    // widgets registered for sidebar
+		"meta":      renderWidget(META_WIDGET, &page, r),       // widgets registered to be displayed under the page title
+		"afterView": renderWidget(AFTER_VIEW_WIDGET, &page, r), // widgets registered to be displayed under the page content in the view page
 	})
 }
 
+// Edit page, gets the page from path, if it doesn't exist it'll use the
+// template.md content as default value
 func GetPageEditHandler(w Response, r Request) Output {
 	vars := VARS(r)
 	page := NewPage(vars["page"])
@@ -70,6 +83,8 @@ func GetPageEditHandler(w Response, r Request) Output {
 		content = template.Content()
 	}
 
+	// Execute all Autocomplete functions and add them to a slice and pass it
+	// down to the view
 	acs := []*Autocomplete{}
 	for _, v := range autocompletes {
 		acs = append(acs, v())
@@ -78,14 +93,15 @@ func GetPageEditHandler(w Response, r Request) Output {
 	return Render("edit", Locals{
 		"title":        page.Name,
 		"action":       page.Name,
-		"rtl":          page.RTL(),
-		"tools":        renderWidget(TOOLS_WIDGET, &page, r),
+		"rtl":          page.RTL(),                           // is it Right-To-Left page?
+		"tools":        renderWidget(TOOLS_WIDGET, &page, r), // render all tools widgets
 		"content":      content,
 		"autocomplete": acs,
 		"csrf":         CSRF(r),
 	})
 }
 
+// Save new content of the page
 func PostPageHandler(w Response, r Request) Output {
 	vars := VARS(r)
 	page := NewPage(vars["page"])
@@ -98,10 +114,19 @@ func PostPageHandler(w Response, r Request) Output {
 // WIDGETS ===================================================
 
 type (
+	// a type used to define list of widgets spaces. it's used to register
+	// widgets to be injected in the view or edit pages
 	widgetSpace int
-	widgetFunc  func(*Page, Request) template.HTML
+	// a function that takes the current page and the HTTP request and returns
+	// the widget. This can be used by extensions to define new widgets to be
+	// rendered in view or edit pages. the extension should define this func
+	// type and register it to be rendered in a specific widgetSpace such as
+	// before or after the page.
+	widgetFunc func(*Page, Request) template.HTML
 )
 
+// List of widgets spaces that extensions can use to register a widgetFunc to
+// inject content into.
 const (
 	TOOLS_WIDGET widgetSpace = iota
 	SIDEBAR_WIDGET
@@ -109,8 +134,13 @@ const (
 	META_WIDGET
 )
 
+// A map to keep track of list of widget functions registered in each widget space
 var widgets = map[widgetSpace][]widgetFunc{}
 
+// Register widget function to be rendered in a specific space before any other
+// widget. functions registered by this function will have higher priority than
+// the rest. this function is needed for example to register the search input
+// before any other links in the sidebar
 func PREPEND_WIDGET(s widgetSpace, f func(*Page, Request) template.HTML) {
 	if _, ok := widgets[s]; !ok {
 		widgets[s] = []widgetFunc{}
@@ -118,6 +148,9 @@ func PREPEND_WIDGET(s widgetSpace, f func(*Page, Request) template.HTML) {
 	widgets[s] = append([]widgetFunc{f}, widgets[s]...)
 }
 
+// Register a function to a widget space. functions registered will be executed
+// in order when rendering view or edit page. the return values of these
+// widgetfuncs will pass down to the template and injected in reserved places.
 func WIDGET(s widgetSpace, f func(*Page, Request) template.HTML) {
 	if _, ok := widgets[s]; !ok {
 		widgets[s] = []widgetFunc{}
@@ -125,6 +158,8 @@ func WIDGET(s widgetSpace, f func(*Page, Request) template.HTML) {
 	widgets[s] = append(widgets[s], f)
 }
 
+// This is used by view and edit routes to render all widgetfuncs registered for
+// specific widget space.
 func renderWidget(s widgetSpace, p *Page, r Request) (o template.HTML) {
 	for _, v := range widgets[s] {
 		o += v(p, r)
@@ -134,6 +169,11 @@ func renderWidget(s widgetSpace, p *Page, r Request) (o template.HTML) {
 
 // HELPERS
 
+// A function that takes time.duration and return a string representation of the
+// duration in human readable way such as "3 seconds ago". "5 hours 30 minutes
+// ago". The precision of this function is 2. which means it returns the largest
+// unit of time possible and the next one after it. for example days + hours, or
+// Hours + minutes or Minutes + seconds...etc
 func ago(t time.Duration) string {
 	const day = time.Hour * 24
 	const week = day * 7
@@ -187,20 +227,30 @@ func ago(t time.Duration) string {
 
 // AUTOCOMPLETE ================================================
 
-type Suggestion struct {
-	Text        string
-	DisplayText string
-}
-
+// Autocomplete defines what character triggeres the autocomplete feature and
+// what is the list to display in this case.
 type Autocomplete struct {
 	StartChar   string
 	Suggestions []*Suggestion
 }
 
+// Suggestions represent an item in the list of autocomplete menu in the edit page
+type Suggestion struct {
+	Text        string // The text that gets injected in the editor if this option is choosen
+	DisplayText string // The display text for this item in the menu. this can be more cosmetic.
+}
+
+// This is a function that returns an auto completer instance. this function
+// should be defined by extensions and registered to be executed when rendering
+// the edit page
 type Autocompleter func() *Autocomplete
 
+// Holds a list of registered autocompleter functions
 var autocompletes = []Autocompleter{}
 
-func AUTOCOMPLETE(a func() *Autocomplete) {
+// this function registers an autocompleter function. it should be used by an
+// extension to register a new autocompleter function. these functions are going
+// to be executed when rendering the edit page.
+func AUTOCOMPLETE(a Autocompleter) {
 	autocompletes = append(autocompletes, a)
 }
--- page.go
+++ page.go
@@ -26,28 +26,48 @@ import (
 )
 
 type (
+	// a Type that represent a page.
 	Page struct {
-		Name string
+		Name string // page name without '.md' extension
 	}
 
-	PageEvent        int
+	// a type used to define events to be used when the page is manipulated for
+	// example modified, renamed, deleted...etc.
+	PageEvent int
+	// a function that handles a page event. this should be implemented by an
+	// extension and then registered. it will get executed when the event is
+	// triggered
 	PageEventHandler func(*Page) error
-	PageEventsMap    map[PageEvent][]PageEventHandler
+	// a map of all handlers functions registered for each page event.
+	PageEventsMap map[PageEvent][]PageEventHandler
 )
 
+// List of page events. extensions can use these events to register a function
+// to be executed when this event is triggered. extensions that require to be
+// notified when the page is created or overwritten or deleted should register
+// an event handler for the interesting events.
 const (
 	BeforeWrite PageEvent = iota
 	AfterWrite
 	AfterDelete
 )
 
+// a List of directories that should be ignored by directory walking function.
+// for example the versioning extension can register `.versions` directory to be
+// ignored
 var ignoredDirs = []*regexp.Regexp{}
 
+// Register a pattern to be ignored when walking directories.
 func IGNORE_DIR(r *regexp.Regexp) {
 	ignoredDirs = append(ignoredDirs, r)
 }
 
+// a map to keep all page events and respective list of event handlers
 var PageEvents = PageEventsMap{}
+
+// The instance of markdown renderer. this is what takes the page content and
+// converts it to HTML. it defines what features to use from goldmark and what
+// options to turn on
 var MarkDownRenderer = goldmark.New(
 	goldmark.WithExtensions(
 		extension.GFM,
@@ -63,6 +83,7 @@ var MarkDownRenderer = goldmark.New(
 	),
 )
 
+// Create an instance of Page with name. if no name is passed it's assumed "index"
 func NewPage(name string) Page {
 	if name == "" {
 		name = "index"
@@ -73,15 +94,20 @@ func NewPage(name string) Page {
 	}
 }
 
+// returns the filename, makes sure it converts slashes to backslashes when
+// needed. this is safe to use when trying to access the file that represent the
+// page
 func (p *Page) FileName() string {
 	return filepath.FromSlash(p.Name) + ".md"
 }
 
+// checks if the page underlying file exists on disk or not.
 func (p *Page) Exists() bool {
 	_, err := os.Stat(p.FileName())
 	return err == nil
 }
 
+// Renders the page content to HTML. it makes sure all preprocessors are called
 func (p *Page) Render() string {
 	content := p.Content()
 	content = preProcess(content)
@@ -94,6 +120,7 @@ func (p *Page) Render() string {
 	return buf.String()
 }
 
+// Reads the underlying file and returns the content
 func (p *Page) Content() string {
 	dat, err := ioutil.ReadFile(p.FileName())
 	if err != nil {
@@ -103,6 +130,7 @@ func (p *Page) Content() string {
 	return string(dat)
 }
 
+// Deletes the file and makes sure it triggers the AfterDelete event
 func (p *Page) Delete() bool {
 	defer PageEvents.Trigger(AfterDelete, p)
 
@@ -116,6 +144,8 @@ func (p *Page) Delete() bool {
 	return true
 }
 
+// Overwrite page content with new content. making sure to trigger before and
+// after write events.
 func (p *Page) Write(content string) bool {
 	PageEvents.Trigger(BeforeWrite, p)
 	defer PageEvents.Trigger(AfterWrite, p)
@@ -131,6 +161,7 @@ func (p *Page) Write(content string) bool {
 	return true
 }
 
+// Return the last modification time of the underlying file
 func (p *Page) ModTime() time.Time {
 	s, err := os.Stat(p.FileName())
 	if err != nil {
@@ -140,14 +171,22 @@ func (p *Page) ModTime() time.Time {
 	return s.ModTime()
 }
 
+// checks if the page includes any arabic character if so it returns true. this
+// is used by the editor to switch direction. it could be useful too for
+// extensions or something
 func (p *Page) RTL() bool {
 	return regexp.MustCompile(`\p{Arabic}`).MatchString(p.Content())
 }
 
+// Parses the page content and returns the Abstract Syntax Tree (AST).
+// extensions can use it to walk the tree and modify it or collect statistics or
+// parts of the page. for example the following "Emoji" function uses it to
+// extract the first emoji.
 func (p *Page) AST() ast.Node {
 	return MarkDownRenderer.Parser().Parse(text.NewReader([]byte(p.Content())))
 }
 
+// Returns the first emoji of the page.
 func (p *Page) Emoji() string {
 	if e := extractEmoji(p.AST()); e != nil {
 		return string(e.Value.Unicode)
@@ -156,6 +195,8 @@ func (p *Page) Emoji() string {
 	return ""
 }
 
+// This is a function that takes an AST node and walks the tree depth first
+// recursively calling itself
 func extractEmoji(n ast.Node) *emojiAst.Emoji {
 	if n.Kind() == emojiAst.KindEmoji {
 		return n.(*emojiAst.Emoji)
@@ -174,6 +215,8 @@ func extractEmoji(n ast.Node) *emojiAst.Emoji {
 	return nil
 }
 
+// this function is useful to iterate on all available pages. many extensions
+// uses it to get all pages and maybe parse them and extract needed information
 func WalkPages(ctx context.Context, f func(*Page)) {
 	filepath.WalkDir(".", func(name string, d fs.DirEntry, err error) error {
 		if d.IsDir() && name == STATIC_DIR_PATH {
@@ -211,6 +254,10 @@ func WalkPages(ctx context.Context, f func(*Page)) {
 	})
 }
 
+// Register an event handler to be executed when PageEvent is triggered.
+// extensions can use this to register hooks under specific page events.
+// extensions that keeps a cached version of the pages list for example needs to
+// register handlers to update its cache
 func (c PageEventsMap) Listen(e PageEvent, h PageEventHandler) {
 	if _, ok := c[e]; !ok {
 		c[e] = []PageEventHandler{}
@@ -219,6 +266,7 @@ func (c PageEventsMap) Listen(e PageEvent, h PageEventHandler) {
 	c[e] = append(c[e], h)
 }
 
+// Trigger event handlers for a specific page event. page methods use this function to trigger all registered handlers when the page is edited or deleted for example.
 func (c PageEventsMap) Trigger(e PageEvent, p *Page) {
 	if _, ok := c[e]; !ok {
 		return
@@ -233,16 +281,26 @@ func (c PageEventsMap) Trigger(e PageEvent, p *Page) {
 
 // PREPROCESSORS =======================
 
+// A preProcessor is a function that takes the whole page content and returns a
+// modified version of the content. extensions should define this type and
+// register is so that when page is rendered it will execute all of them in
+// order like a pipeline each function output is passed as an input to the next.
+// at the end the last preprocessor output is then rendered to HTML
 type (
 	preProcessor func(string) string
 )
 
+// List of registered preprocessor functions
 var (
 	preProcessors = []preProcessor{}
 )
 
+// Register a PREPROCESSOR function. extensions should use this function to
+// register a preprocessor.
 func PREPROCESSOR(f preProcessor) { preProcessors = append(preProcessors, f) }
 
+// This function take the page content and pass it through all registered
+// preprocessors and return the last preprocessor output to the caller
 func preProcess(content string) string {
 	for _, v := range preProcessors {
 		content = v(content)
