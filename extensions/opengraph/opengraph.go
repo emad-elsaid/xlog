df3c774bf04b3ec7661d668ec622357636fe68c3
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2022-12-10 14:39:08 +0100

    reiterate on the twitter card meta data

--- extensions/opengraph/opengraph.go
+++ extensions/opengraph/opengraph.go
@@ -16,7 +16,7 @@ func init() {
 func opengraphTags(p Page, r Request) template.HTML {
 	tags := `
 <meta property="og:type" content="article" />
-<meta name="twitter:card" content="summary" />
+<meta name="twitter:card" content="summary_large_image" />
 `
 	name := p.Name()
 	if p.Name() == INDEX {
@@ -24,9 +24,11 @@ func opengraphTags(p Page, r Request) template.HTML {
 	}
 
 	tags += fmt.Sprintf(`<meta property="og:title" content="%s %s" />`, p.Emoji(), template.JSEscapeString(name))
+	tags += fmt.Sprintf(`<meta name="twitter:title" content="%s %s" />`, p.Emoji(), template.JSEscapeString(name))
 
 	if image, ok := FindInAST[*ast.Image](p.AST(), ast.KindImage); ok {
 		tags += fmt.Sprintf(`<meta property="og:image" content="%s" />`, template.JSEscapeString(string(image.Destination)))
+		tags += fmt.Sprintf(`<meta name="twitter:image" content="%s" />`, template.JSEscapeString(string(image.Destination)))
 	}
 
 	return template.HTML(tags)
