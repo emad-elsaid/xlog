98c6b3cf2040af810a91b936432efb9c330fd339
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2023-06-11 16:59:22 +0200

    add github search issues

--- docs/Github.md
+++ docs/Github.md
@@ -2,3 +2,7 @@ Code is hosted on Github repository.
 
 
 https://github.com/emad-elsaid/xlog
+
+# Open pull requests
+
+/github-search-issues repo:emad-elsaid/xlog is:pull-request is:open
---
+++ extensions/github/api.go
@@ -0,0 +1,90 @@
+package github
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"html/template"
+	"os"
+	"strings"
+
+	"github.com/emad-elsaid/xlog"
+	"github.com/emad-elsaid/xlog/extensions/shortcode"
+	"github.com/google/go-github/v53/github"
+	"golang.org/x/oauth2"
+)
+
+var githubTokenPossibleVariables = []string{"GITHUB_TOKEN", "GITHUB_API_TOKEN"}
+var tokenNotAvailable = errors.New("Github token env variable not found in any of: " + strings.Join(githubTokenPossibleVariables, ", "))
+var perPage = 100
+
+func init() {
+	shortcode.ShortCode("github-search-issues", seachIssuesShortcode)
+}
+
+func seachIssuesShortcode(in xlog.Markdown) template.HTML {
+	return template.HTML(issues(context.Background(), string(in)))
+}
+
+func token() (string, error) {
+	for _, v := range githubTokenPossibleVariables {
+		value := os.Getenv(v)
+		if len(value) > 0 {
+			return value, nil
+		}
+	}
+
+	return "", tokenNotAvailable
+}
+
+func client() (*github.Client, error) {
+	token, err := token()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	return github.NewClient(tc), nil
+}
+
+func issues(ctx context.Context, query string) string {
+	client, err := client()
+	if err != nil {
+		return err.Error()
+	}
+
+	result, _, err := client.Search.Issues(ctx, query, &github.SearchOptions{
+		ListOptions: github.ListOptions{
+			PerPage: perPage,
+		},
+	})
+	if err != nil {
+		return err.Error()
+	}
+
+	if len(result.Issues) == 0 {
+		return fmt.Sprintf("No results for query: %s", query)
+	}
+
+	issues := "<ul>"
+	for _, i := range result.Issues {
+		assignee := i.GetUser()
+
+		issues += fmt.Sprintf(`<li>
+<span class="icon-text" >
+	<figure class="icon image is-24x24 my-0 mx-2">
+	  <img src="%s" class="is-rounded">
+	</figure>
+	<a href="%s">%s</a>
+</span>
+</li>`, assignee.GetAvatarURL(), i.GetURL(), i.GetTitle())
+	}
+	issues += "</ul>"
+
+	return issues
+}
