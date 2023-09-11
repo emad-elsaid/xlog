6e4b11a2b80455dcc84db93145c27e7ac55b17d8
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-09-12 02:26:49 +0300

    notfoundpage flag

--- flags.go
+++ flags.go
@@ -6,12 +6,13 @@ import (
 )
 
 var (
-	SOURCE   string // path to markdown files directory
-	BUILD    string // path to write built files
-	READONLY bool   // is xlog in readonly mode
-	SITENAME string // name of knowledgebase
-	SIDEBAR  bool   // is sidebar displayed
-	INDEX    string // name of the index page markdown file
+	SOURCE         string // path to markdown files directory
+	BUILD          string // path to write built files
+	READONLY       bool   // is xlog in readonly mode
+	SITENAME       string // name of knowledgebase
+	SIDEBAR        bool   // is sidebar displayed
+	INDEX          string // name of the index page markdown file
+	NOT_FOUND_PAGE string // name of the index page markdown file
 )
 
 func init() {
@@ -23,6 +24,7 @@ func init() {
 	flag.StringVar(&BUILD, "build", "", "Build all pages as static site in this directory")
 	flag.StringVar(&SITENAME, "sitename", "XLOG", "Site name is the name that appears on the header beside the logo and in the title tag")
 	flag.StringVar(&INDEX, "index", "index", "Index file name used as home page")
+	flag.StringVar(&NOT_FOUND_PAGE, "notfoundpage", "404", "Custom not found page")
 	flag.BoolVar(&READONLY, "readonly", false, "Should xlog hide write operations, read-only means all write operations will be disabled")
 	flag.BoolVar(&SIDEBAR, "sidebar", true, "Should render sidebar.")
 }
