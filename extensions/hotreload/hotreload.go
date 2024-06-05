a8997151adaab225764899e6a8b8a7d2624abfcf
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2024-06-05 22:07:54 +0200

    use `/+/hotreload` instead of `/ws` and extract Noop

--- extensions/hotreload/hotreload.go
+++ extensions/hotreload/hotreload.go
@@ -1,91 +1,88 @@
 package hotreload
 
 import (
-    "fmt"
-    "html/template"
-    "log"
-    "sync"
+	"fmt"
+	"html/template"
+	"log"
+	"sync"
 
-    . "github.com/emad-elsaid/xlog"
-    "github.com/gorilla/websocket"
+	"github.com/emad-elsaid/xlog"
+	. "github.com/emad-elsaid/xlog"
+	"github.com/gorilla/websocket"
 )
 
 var (
-    upgrader     = websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
-    clients      = make(map[*websocket.Conn]bool)
-    clientsMutex sync.Mutex
+	upgrader     = websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
+	clients      = make(map[*websocket.Conn]bool)
+	clientsMutex sync.Mutex
 )
 
 func init() {
-    Listen(Changed, NotifyPageChange)
-    Get(`/ws`, handleWebSocket)
-    RegisterWidget(AFTER_VIEW_WIDGET, 0, clientWidget)
+	Listen(Changed, NotifyPageChange)
+	Get(`/+/hotreload`, handleWebSocket)
+	RegisterWidget(AFTER_VIEW_WIDGET, 0, clientWidget)
 }
-
 
 func NotifyPageChange(p Page) error {
-    if !p.Exists() {
-        return nil
-    }
+	if !p.Exists() {
+		return nil
+	}
 
-    message := map[string]string{"url": fmt.Sprintf("/%s", p.Name())}
-    log.Printf("Page %s changed from notifiy", p.Name())
+	message := map[string]string{"url": fmt.Sprintf("/%s", p.Name())}
 
-    clientsMutex.Lock()
-    defer clientsMutex.Unlock()
+	clientsMutex.Lock()
+	defer clientsMutex.Unlock()
 
-    for client := range clients {
-        err := client.WriteJSON(message)
-        if err != nil {
-            log.Printf("Error sending message to client: %v\n", err)
-            client.Close()
-            delete(clients, client)
-        }
-    }
-    return nil
+	for client := range clients {
+		err := client.WriteJSON(message)
+		if err != nil {
+			client.Close()
+			delete(clients, client)
+		}
+	}
+	return nil
 }
 
-func nop(w Response, r Request) {
-}
 func handleWebSocket(w Response, r Request) Output {
-    if !READONLY {
-        return NoContent()
-    }
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println("Upgrade error:", err)
-        return BadRequest(err.Error())
-    }
+	if READONLY {
+		return NoContent()
+	}
 
-    // keep connection open
-    go func() {
-        defer func() {
-            clientsMutex.Lock()
-            delete(clients, conn)
-            clientsMutex.Unlock()
-            conn.Close()
-        }()
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return BadRequest(err.Error())
+	}
 
-        for {
-            mt, _, err := conn.ReadMessage()
-            if err != nil || mt ==  websocket.CloseMessage {
-                break
-            }
-        }
-    }()
+	// keep connection open
+	go func() {
+		defer func() {
+			clientsMutex.Lock()
+			delete(clients, conn)
+			clientsMutex.Unlock()
+			conn.Close()
+		}()
 
-    clientsMutex.Lock()
-    clients[conn] = true
-    clientsMutex.Unlock()
-    log.Printf("Connection arrived. curr cons: %d", len(clients))
-    return nop
+		for {
+			mt, _, err := conn.ReadMessage()
+			if err != nil || mt == websocket.CloseMessage {
+				break
+			}
+		}
+	}()
+
+	clientsMutex.Lock()
+	clients[conn] = true
+	clientsMutex.Unlock()
+
+	return xlog.Noop
 }
 
 // TODO use same HOST and PORT than server
 const clientScript = `
     <script>
     (() => {
-        const socketUrl = 'ws://'+window.location.host+'/ws';
+        const socketUrl = 'ws://'+window.location.host+'/+/hotreload';
         let socket = new WebSocket(socketUrl);
         socket.addEventListener('message', (evt) => {
             let data = JSON.parse(evt.data)
@@ -96,10 +93,9 @@ const clientScript = `
     `
 
 func clientWidget(p Page) template.HTML {
-    // return template.HTML(fmt.Sprint(clientScript, template.JSEscapeString(p.Name())))
-    if !READONLY {
-        return template.HTML(clientScript)
-    }
+	if !READONLY {
+		return template.HTML(clientScript)
+	}
 
-    return ""
+	return ""
 }
