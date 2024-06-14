304d04e379b4cb2088a2ae212d8654fe6e03aa51
Juan Pablo <juanolon@gmail.com>
2024-06-14 16:28:55 +0200

    fix frontmatter strip, whenever a '---' was in the document

--- extensions/metadata/metadata.go
+++ extensions/metadata/metadata.go
@@ -4,43 +4,60 @@ import (
 	"bufio"
 	"strings"
 
+	"github.com/emad-elsaid/xlog"
 	. "github.com/emad-elsaid/xlog"
+	"gopkg.in/yaml.v2"
 )
 
 func init() {
 	RegisterPreprocessor(stripYAML)
 }
 
-// TODO move to an extension. see link_preview
 func stripYAML(content Markdown) Markdown {
 	reader := strings.NewReader(string(content))
 
 	scanner := bufio.NewScanner(reader)
 	var body strings.Builder
+	var y strings.Builder
 	inFrontMatter := false
+	frontMatterFound := false
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		trimmedLine := strings.TrimSpace(line)
-
-		if trimmedLine == "---" {
+		if strings.TrimSpace(line) == "---" {
 			if inFrontMatter {
 				// End of front matter
 				inFrontMatter = false
-			} else {
+				frontMatterFound = true
+			} else if !frontMatterFound {
 				// Start of front matter
 				inFrontMatter = true
+			} else {
+				body.WriteString(line)
+				body.WriteString("\n")
 			}
-		} else if !inFrontMatter {
-			// Append lines not part of the front matter
-			body.WriteString(line)
-			body.WriteString("\n")
+		} else {
+			if inFrontMatter {
+				y.WriteString(line)
+				y.WriteString("\n")
+			} else if !inFrontMatter {
+				body.WriteString(line)
+				body.WriteString("\n")
+			}
+
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		// ignore error, and just return original content
+		return content
+	}
+
+	var meta xlog.Metadata
+	// only strip if valid yaml
+	err := yaml.Unmarshal([]byte(y.String()), &meta)
+	if err != nil {
 		return content
 	}
 	return Markdown(body.String())
