0456426843438777e20a7956a0ce87b124097633
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2024-12-22 21:26:13 +0100

    refactoring for properties

--- property.go
+++ property.go
@@ -5,33 +5,39 @@ package xlog
 type Property interface {
 	// Icon returns the fontawesome icon class name or emoji
 	Icon() string
-	// Name returns the link text
+	// Name returns the name of the property
 	Name() string
+	// Value returns the value of the property
+	Value() any
 }
 
-var props = []func(Page) []Property{defaultProps}
+var propsSources = []func(Page) []Property{defaultProps}
 
 // RegisterProperty registers a function that returns a set of properties for
 // the page
 func RegisterProperty(a func(Page) []Property) {
-	props = append(props, a)
+	propsSources = append(propsSources, a)
 }
 
 // Properties return a list of properties for a page. It executes all functions
 // registered with RegisterProperty and collect results in one slice. Can be
 // passed to the view to render a page properties
-func Properties(p Page) []Property {
-	ps := []Property{}
-	for pr := range props {
-		ps = append(ps, props[pr](p)...)
+func Properties(p Page) map[string]Property {
+	ps := map[string]Property{}
+	for _, source := range propsSources {
+		for _, pr := range source(p) {
+			ps[pr.Name()] = pr
+		}
 	}
+
 	return ps
 }
 
 type lastUpdateProp struct{ page Page }
 
 func (a lastUpdateProp) Icon() string { return "fa-solid fa-clock" }
-func (a lastUpdateProp) Name() string { return ago(a.page.ModTime()) }
+func (a lastUpdateProp) Name() string { return "Modified" }
+func (a lastUpdateProp) Value() any   { return ago(a.page.ModTime()) }
 
 func defaultProps(p Page) []Property {
 	return []Property{
