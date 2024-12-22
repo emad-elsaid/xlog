94b3dee0fa49d7d47f490a9c19a7b2f2e301c634
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2024-12-22 22:11:13 +0100

    join priority list with widgets

--- widgets.go
+++ widgets.go
@@ -2,6 +2,8 @@ package xlog
 
 import (
 	"html/template"
+	"iter"
+	"sort"
 )
 
 type (
@@ -53,4 +55,35 @@ func RenderWidget(s WidgetSpace, p Page) (o template.HTML) {
 		o += f(p)
 	}
 	return
+}
+
+type priorityItem[T any] struct {
+	Item     T
+	Priority float32
+}
+
+type priorityList[T any] struct {
+	items []priorityItem[T]
+}
+
+func (pl *priorityList[T]) Add(item T, priority float32) {
+	pl.items = append(pl.items, priorityItem[T]{Item: item, Priority: priority})
+	pl.sortByPriority()
+}
+
+func (pl *priorityList[T]) sortByPriority() {
+	sort.Slice(pl.items, func(i, j int) bool {
+		return pl.items[i].Priority < pl.items[j].Priority
+	})
+}
+
+// An iterator over all items
+func (pl *priorityList[T]) All() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for _, v := range pl.items {
+			if !yield(v.Item) {
+				return
+			}
+		}
+	}
 }
