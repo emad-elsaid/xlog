209f94ce7a1f9642f760d5f5251df675d5b001ef
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2025-01-13 08:35:21 +0100

    cache pages hashtags to improve performance

--- extensions/hashtags/hashtags.go
+++ extensions/hashtags/hashtags.go
@@ -25,20 +25,30 @@ import (
 var templates embed.FS
 
 func init() {
-	RegisterExtension(Hashtags{})
+	h := Hashtags{
+		pages: make(map[Page][]*HashTag),
+	}
+
+	RegisterExtension(&h)
 }
 
-type Hashtags struct{}
+type Hashtags struct {
+	pages map[Page][]*HashTag
+	mu    sync.Mutex
+}
 
-func (Hashtags) Name() string { return "hashtags" }
-func (Hashtags) Init() {
-	Get(`/+/tags`, tagsHandler)
-	Get(`/+/tag/{tag}`, tagHandler)
-	RegisterWidget(WidgetAfterView, 1, relatedPages)
+func (*Hashtags) Name() string { return "hashtags" }
+func (h *Hashtags) Init() {
+	Get(`/+/tags`, h.tagsHandler)
+	Get(`/+/tag/{tag}`, h.tagHandler)
+	RegisterWidget(WidgetAfterView, 1, h.relatedPages)
 	RegisterBuildPage("/+/tags", true)
 	RegisterLink(links)
 	RegisterTemplate(templates, "templates")
-	shortcode.RegisterShortCode("hashtag-pages", shortcode.ShortCode{Render: hashtagPages})
+	shortcode.RegisterShortCode("hashtag-pages", shortcode.ShortCode{Render: h.hashtagPages})
+
+	Listen(PageChanged, h.PageChanged)
+	Listen(PageDeleted, h.PageDeleted)
 
 	MarkdownConverter().Renderer().AddOptions(renderer.WithNodeRenderers(
 		util.Prioritized(&HashTag{}, 0),
@@ -46,6 +56,142 @@ func (Hashtags) Init() {
 	MarkdownConverter().Parser().AddOptions(parser.WithInlineParsers(
 		util.Prioritized(&HashTag{}, 999),
 	))
+}
+
+func (h *Hashtags) PageChanged(p Page) error {
+	delete(h.pages, p)
+
+	return nil
+}
+
+func (h *Hashtags) PageDeleted(p Page) error {
+	return h.PageChanged(p)
+}
+
+func (h *Hashtags) hashtagsFor(p Page) []*HashTag {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if tags, ok := h.pages[p]; ok {
+		return tags
+	}
+
+	_, tree := p.AST()
+	tags := FindAllInAST[*HashTag](tree)
+	h.pages[p] = tags
+
+	return tags
+}
+
+func (h *Hashtags) tagsHandler(r Request) Output {
+	tags := map[string][]Page{}
+	var lck sync.Mutex
+
+	EachPage(r.Context(), func(a Page) {
+		set := map[string]bool{}
+		_, tree := a.AST()
+		hashes := FindAllInAST[*HashTag](tree)
+		for _, v := range hashes {
+			val := strings.ToLower(string(v.value))
+
+			// don't use same tag twice for same page
+			if _, ok := set[val]; ok {
+				continue
+			}
+
+			set[val] = true
+
+			lck.Lock()
+			if ps, ok := tags[val]; ok {
+				tags[val] = append(ps, a)
+			} else {
+				tags[val] = []Page{a}
+			}
+			lck.Unlock()
+		}
+	})
+
+	return Render("tags", Locals{
+		"page": DynamicPage{NameVal: "Hashtags"},
+		"tags": tags,
+	})
+}
+
+func (h *Hashtags) tagHandler(r Request) Output {
+	tag := r.PathValue("tag")
+
+	return Render("tag", Locals{
+		"page":  DynamicPage{NameVal: "#" + tag},
+		"pages": h.tagPages(r.Context(), tag),
+	})
+}
+
+func (h *Hashtags) tagPages(ctx context.Context, hashtag string) []Page {
+	uniqHandle := unique.Make(strings.ToLower(hashtag))
+
+	return MapPage(ctx, func(p Page) Page {
+		if p.Name() == Config.Index {
+			return nil
+		}
+
+		tags := h.hashtagsFor(p)
+		for _, t := range tags {
+			if uniqHandle == t.unique {
+				return p
+			}
+		}
+
+		return nil
+	})
+}
+
+func (h *Hashtags) relatedPages(p Page) template.HTML {
+	if p.Name() == Config.Index {
+		return ""
+	}
+
+	_, tree := p.AST()
+	found_hashtags := FindAllInAST[*HashTag](tree)
+	hashtags := map[unique.Handle[string]]bool{}
+	for _, v := range found_hashtags {
+		hashtags[v.unique] = true
+	}
+
+	pages := MapPage(context.Background(), func(rp Page) Page {
+		if rp.Name() == p.Name() {
+			return nil
+		}
+
+		_, tree := rp.AST()
+		page_hashtags := FindAllInAST[*HashTag](tree)
+		for _, h := range page_hashtags {
+			if _, ok := hashtags[h.unique]; ok {
+				return rp
+			}
+		}
+
+		return nil
+	})
+
+	return Partial("related-hashtags-pages", Locals{
+		"pages": pages,
+	})
+}
+
+func (h *Hashtags) hashtagPages(hashtag Markdown) template.HTML {
+	hashtag_value := strings.Trim(string(hashtag), "# \n")
+	pages := h.tagPages(context.Background(), hashtag_value)
+
+	slices.SortFunc(pages, func(a, b Page) int {
+		if modtime := b.ModTime().Compare(a.ModTime()); modtime != 0 {
+			return modtime
+		}
+
+		return strings.Compare(a.Name(), b.Name())
+	})
+
+	output := Partial("hashtag-pages", Locals{"pages": pages})
+	return template.HTML(output)
 }
 
 func links(Page) []Command {
@@ -131,116 +277,4 @@ func renderHashtag(writer util.BufWriter, source []byte, n ast.Node, entering bo
 	RegisterBuildPage(fmt.Sprintf("/+/tag/%s", tag.value), true)
 	RegisterBuildPage(fmt.Sprintf("/+/tag/%s", strings.ToLower(string(tag.value))), true)
 	return ast.WalkContinue, nil
-}
-
-func tagsHandler(r Request) Output {
-	tags := map[string][]Page{}
-	var lck sync.Mutex
-
-	EachPage(r.Context(), func(a Page) {
-		set := map[string]bool{}
-		_, tree := a.AST()
-		hashes := FindAllInAST[*HashTag](tree)
-		for _, v := range hashes {
-			val := strings.ToLower(string(v.value))
-
-			// don't use same tag twice for same page
-			if _, ok := set[val]; ok {
-				continue
-			}
-
-			set[val] = true
-
-			lck.Lock()
-			if ps, ok := tags[val]; ok {
-				tags[val] = append(ps, a)
-			} else {
-				tags[val] = []Page{a}
-			}
-			lck.Unlock()
-		}
-	})
-
-	return Render("tags", Locals{
-		"page": DynamicPage{NameVal: "Hashtags"},
-		"tags": tags,
-	})
-}
-
-func tagHandler(r Request) Output {
-	tag := r.PathValue("tag")
-
-	return Render("tag", Locals{
-		"page":  DynamicPage{NameVal: "#" + tag},
-		"pages": tagPages(r.Context(), tag),
-	})
-}
-
-func tagPages(ctx context.Context, hashtag string) []Page {
-	uniqHandle := unique.Make(strings.ToLower(hashtag))
-
-	return MapPage(ctx, func(p Page) Page {
-		if p.Name() == Config.Index {
-			return nil
-		}
-
-		_, tree := p.AST()
-		tags := FindAllInAST[*HashTag](tree)
-		for _, t := range tags {
-			if uniqHandle == t.unique {
-				return p
-			}
-		}
-
-		return nil
-	})
-}
-
-func relatedPages(p Page) template.HTML {
-	if p.Name() == Config.Index {
-		return ""
-	}
-
-	_, tree := p.AST()
-	found_hashtags := FindAllInAST[*HashTag](tree)
-	hashtags := map[unique.Handle[string]]bool{}
-	for _, v := range found_hashtags {
-		hashtags[v.unique] = true
-	}
-
-	pages := MapPage(context.Background(), func(rp Page) Page {
-		if rp.Name() == p.Name() {
-			return nil
-		}
-
-		_, tree := rp.AST()
-		page_hashtags := FindAllInAST[*HashTag](tree)
-		for _, h := range page_hashtags {
-			if _, ok := hashtags[h.unique]; ok {
-				return rp
-			}
-		}
-
-		return nil
-	})
-
-	return Partial("related-hashtags-pages", Locals{
-		"pages": pages,
-	})
-}
-
-func hashtagPages(hashtag Markdown) template.HTML {
-	hashtag_value := strings.Trim(string(hashtag), "# \n")
-	pages := tagPages(context.Background(), hashtag_value)
-
-	slices.SortFunc(pages, func(a, b Page) int {
-		if modtime := b.ModTime().Compare(a.ModTime()); modtime != 0 {
-			return modtime
-		}
-
-		return strings.Compare(a.Name(), b.Name())
-	})
-
-	output := Partial("hashtag-pages", Locals{"pages": pages})
-	return template.HTML(output)
 }
