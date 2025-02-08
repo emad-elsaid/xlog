8b7d8bd9e45da90d51fb6cccaab57b0897283c55
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2025-02-08 13:03:32 +0100

    add a flag to enable the feature for tables that has more than 100 rows by default.

--- extensions/sql_table/extension.go
+++ extensions/sql_table/extension.go
@@ -2,15 +2,22 @@ package sql_table
 
 import (
 	"embed"
+	"flag"
+	"fmt"
 	"html/template"
 
+	"github.com/emad-elsaid/types"
 	"github.com/emad-elsaid/xlog"
+	east "github.com/yuin/goldmark/extension/ast"
 )
 
 //go:embed js
 var js embed.FS
 
+var sqlTableThreshold int
+
 func init() {
+	flag.IntVar(&sqlTableThreshold, "sql-table.threshold", 100, "If a table rows is more than this threshold it'll allow users to query it with SQL")
 	xlog.RegisterExtension(Extension{})
 }
 
@@ -25,6 +32,29 @@ func (Extension) Init() {
 }
 
 func script(p xlog.Page) template.HTML {
+	if p == nil {
+		return ""
+	}
+
+	_, a := p.AST()
+	if a == nil {
+		return ""
+	}
+
+	tables := xlog.FindAllInAST[*east.Table](a)
+	if len(tables) == 0 {
+		return ""
+	}
+
+	largeTableFound := types.Slice[*east.Table](tables).Any(func(t *east.Table) bool {
+		return len(xlog.FindAllInAST[*east.TableRow](t)) >= sqlTableThreshold
+	})
+	if !largeTableFound {
+		return ""
+	}
+
 	o, _ := js.ReadFile("js/sql_table.html")
+	o = append(o, []byte(fmt.Sprintf("<script>const sqlTableThreshold = %d</script>", sqlTableThreshold))...)
+
 	return template.HTML(o)
 }
