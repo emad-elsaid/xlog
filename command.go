a4bd2e30cb95d0ee73f31c6c76f704c7f1051797
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2025-07-05 16:30:49 +0200

    refactor

--- command.go
+++ command.go
@@ -17,6 +17,15 @@ func (app *App) RegisterCommand(c func(Page) []Command) {
 	app.commands = append(app.commands, c)
 }
 
+// Commands returns the list of commands for a page
+func (app *App) Commands(p Page) []Command {
+	cmds := []Command{}
+	for _, c := range app.commands {
+		cmds = append(cmds, c(p)...)
+	}
+	return cmds
+}
+
 // RegisterQuickCommand registers a new quick command
 func (app *App) RegisterQuickCommand(c func(Page) []Command) {
 	app.quickCommands = append(app.quickCommands, c)
@@ -27,19 +36,8 @@ func (app *App) RegisterLink(l func(Page) []Command) {
 	app.links = append(app.links, l)
 }
 
-// Commands returns the list of commands for a page
-func (app *App) Commands(p Page) []Command {
-
-	cmds := []Command{}
-	for _, c := range app.commands {
-		cmds = append(cmds, c(p)...)
-	}
-	return cmds
-}
-
 // QuickCommands returns the list of quick commands for a page
 func (app *App) QuickCommands(p Page) []Command {
-
 	cmds := []Command{}
 	for _, c := range app.quickCommands {
 		cmds = append(cmds, c(p)...)
@@ -49,7 +47,6 @@ func (app *App) QuickCommands(p Page) []Command {
 
 // Links returns a list of links for a Page
 func (app *App) Links(p Page) []Command {
-
 	cmds := []Command{}
 	for _, l := range app.links {
 		cmds = append(cmds, l(p)...)
