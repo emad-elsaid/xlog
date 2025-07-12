5d788a471d05487069eb31e27aed71f082309641
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2025-07-12 19:36:53 +0200

    remove GetApp from sitemap

--- extensions/sitemap/sitemap.go
+++ extensions/sitemap/sitemap.go
@@ -14,21 +14,23 @@ var SITEMAP_DOMAIN string
 func init() {
 	app := GetApp()
 	flag.StringVar(&SITEMAP_DOMAIN, "sitemap.domain", "", "domain name without protocol or trailing / to use for sitemap loc")
-	app.RegisterExtension(Sitemap{})
+	app.RegisterExtension(new(Sitemap))
 }
 
-type Sitemap struct{}
+type Sitemap struct {
+	app *App
+}
 
-func (Sitemap) Name() string { return "sitemap" }
-func (Sitemap) Init(app *App) {
-	app.Get(`/sitemap.xml`, handler)
+func (*Sitemap) Name() string { return "sitemap" }
+func (s *Sitemap) Init(app *App) {
+	s.app = app
+	app.Get(`/sitemap.xml`, s.handler)
 	app.RegisterBuildPage("/sitemap.xml", false)
 }
 
-func handler(r Request) Output {
+func (s *Sitemap) handler(r Request) Output {
 	output := []string{`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`}
-	app := GetApp()
-	output = append(output, MapPage(app, r.Context(), func(p Page) string {
+	output = append(output, MapPage(s.app, r.Context(), func(p Page) string {
 		return fmt.Sprintf("<url><loc>https://%s/%s</loc></url>", SITEMAP_DOMAIN, url.PathEscape(p.Name()))
 	})...)
 
