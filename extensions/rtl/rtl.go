7bd906d88af6d21e74e9727decb81176533eeb8b
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2025-07-12 18:17:57 +0200

    restore rtl

--- extensions/rtl/rtl.go
+++ extensions/rtl/rtl.go
@@ -1,13 +1,16 @@
 package rtl
 
 import (
-	"html/template"
-
+	"github.com/emad-elsaid/xlog"
 	. "github.com/emad-elsaid/xlog"
+	"github.com/emad-elsaid/xlog/markdown/ast"
+	"github.com/emad-elsaid/xlog/markdown/parser"
+	"github.com/emad-elsaid/xlog/markdown/text"
+	"github.com/emad-elsaid/xlog/markdown/util"
 )
 
 func init() {
-	app := GetApp()
+	app := xlog.GetApp()
 	app.RegisterExtension(RTL{})
 }
 
@@ -15,10 +18,31 @@ type RTL struct{}
 
 func (RTL) Name() string { return "rtl" }
 func (RTL) Init() {
-	app := GetApp()
-	app.RegisterWidget(WidgetHead, 0, rtlWidget)
+	MarkdownConverter().Parser().AddOptions(
+		parser.WithASTTransformers(
+			util.Prioritized(addDirAuto{}, 0),
+		),
+	)
 }
 
-func rtlWidget(Page) template.HTML {
-	return template.HTML(`<link rel="stylesheet" href="/public/rtl.css">`)
+type addDirAuto struct{}
+
+func (t addDirAuto) Transform(doc *ast.Document, reader text.Reader, pc parser.Context) {
+	tags := []ast.Node{}
+
+	ast.Walk(doc, func(node ast.Node, entering bool) (ast.WalkStatus, error) {
+		kind := node.Kind()
+		if kind == ast.KindParagraph ||
+			kind == ast.KindHeading ||
+			kind == ast.KindList ||
+			kind == ast.KindBlockquote {
+			tags = append(tags, node)
+		}
+
+		return ast.WalkContinue, nil
+	})
+
+	for _, t := range tags {
+		t.SetAttributeString("dir", []byte("auto"))
+	}
 }
